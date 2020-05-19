f4f69008b81b39fa56b1235229d224d60e646259
illidan18 <1229166118@qq.com>
2020-05-19 10:40:27 +0800

    fix:修复success_killed表中create_time为空的问题

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE success_killed(
   `seckill_id` BIGINT NOT NULL COMMENT '秒杀商品ID',
   `user_phone` BIGINT NOT NULL COMMENT '用户手机号',
   `state` TINYINT NOT NULL DEFAULT -1 COMMENT '状态标识:-1:无效 0:成功 1:已付款 2:已发货',
-  `create_time` TIMESTAMP NOT NULL COMMENT '创建时间',
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY(seckill_id,user_phone),/*联合主键*/
   KEY idx_create_time(create_time)
 )ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
